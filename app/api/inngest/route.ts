2af7bc5567170834509f5755090ea6ee3e54f790
Proactii <hello@proactii.com>
2026-08-04 11:53:05 +0530

    inngest not work so add console

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -14,6 +14,12 @@ import {
   sendInactiveUserEmails,
 } from "@/lib/inngest/functions";
 
+console.log("INNGEST ENV CHECK:", {
+  eventKey: !!process.env.INNGEST_EVENT_KEY,
+  signingKey: !!process.env.INNGEST_SIGNING_KEY,
+});
+
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
